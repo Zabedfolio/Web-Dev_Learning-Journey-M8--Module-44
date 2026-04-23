4273fe3414b5a7f115980e8e2c4c38fde353e076
Zabedfolio <zabedfolio@gmail.com>
2026-04-23 08:12:52 +0600

    card added

---
+++ src/app/tasks/page.jsx
@@ -0,0 +1,20 @@
+import TasksCard from '@/components/TasksCard';
+import { getTasks } from '@/lib/tasks';
+import React from 'react';
+
+const TasksPage = async() => {
+
+    const tasks = await getTasks();
+    return (
+        <div>
+            <h2>Tasks: {tasks.length}</h2>
+            <div className='grid grid-cols-3 gap-3'>
+                {
+                    tasks.map(task => <TasksCard key={task.id} task={task}></TasksCard>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default TasksPage;
---
+++ src/components/TasksCard.jsx
@@ -0,0 +1,23 @@
+// import {CircleDollar} from "@gravity-ui/icons";
+import {Card} from "@heroui/react";
+
+const TasksCard = ({task}) => {
+
+    const {title, description,status}=task;
+    return (
+        <Card className="">
+      {/* <CircleDollar aria-label="Dollar sign icon" className="text-primary size-6" role="img" /> */}
+      <Card.Header>
+        <Card.Title className="text-xl text-black">{title}</Card.Title>
+        <Card.Description>
+          {description}
+        </Card.Description>
+      </Card.Header>
+      <Card.Footer className="text-black">
+        {status}
+      </Card.Footer>
+    </Card>
+    );
+};
+
+export default TasksCard;
